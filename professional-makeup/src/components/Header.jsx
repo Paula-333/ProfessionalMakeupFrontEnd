1df0e0ecb8d57b73811a5c3ee99647c9aea02598
Paula <PaulaLoveSmile@MacBook-Pro-de-Paulaa.local>
2021-01-11 18:35:48 +0100

    probando header

---
+++ professional-makeup/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Navbar, Nav } from 'react-bootstrap';
+
+import './Header.css';
+
+const Header = () => {
+      
+    return (
+
+        <header className="header">   
+        <div> <img alt="" className="foto"/></div>         
+        </header>
+    )
+}
+export default Header;

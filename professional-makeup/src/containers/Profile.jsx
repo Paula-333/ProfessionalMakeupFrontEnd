e32ed29579d03c234a50ccc61e5b44b6169270ca
Paula <PaulaLoveSmile@MacBook-Pro-de-Paulaa.local>
2021-01-15 14:49:49 +0100

    Añadido diseño Profile

--- professional-makeup/src/containers/Profile.jsx
+++ professional-makeup/src/containers/Profile.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import { Table} from "react-bootstrap";
+import {Container, Table} from "react-bootstrap";
 
 import './styles.css';
 
-const Profile = () => {}
+const Profile = () => {
+    return (
+        <Container style={{width: '50vw'}} className={"mt-5"}>
+            <Table style={{ backgroundColor:'#faebe5', color:'#883f4f'}} striped bordered hover variant="light">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nombre</th>
+                    <th>Cita</th>
+                    <th>Maquillaje</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+            </tbody>
+        </Table>
+        </Container>
+    )
+}
 
 export default Profile;
